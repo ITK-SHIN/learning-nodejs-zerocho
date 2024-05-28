a9ca05018bc25c7e162f3b8419b2ccac7bf879f2
Shin Sangwoo <bano112@naver.com>
2024-05-28 17:21:01 +0900

    3.7 : event2

---
+++ lecture/3-7_event/event.js
@@ -0,0 +1,39 @@
+const EventEmitter = require("events"); // events 모듈을 불러옵니다.
+
+const myEvent = new EventEmitter(); // EventEmitter 인스턴스를 생성합니다.
+myEvent.addListener("event1", () => {
+  console.log("이벤트 1");
+});
+
+myEvent.on("event2", () => {
+  console.log("이벤트 2");
+});
+
+myEvent.on("event2", () => {
+  console.log("이벤트 2 추가");
+});
+
+myEvent.once("event3", () => {
+  console.log("이벤트 3");
+}); // 한 번만 실행됨
+
+myEvent.emit("event1"); // 이벤트 호출
+myEvent.emit("event2"); // 이벤트 호출
+
+myEvent.emit("event3"); // 이벤트 호출
+myEvent.emit("event3"); // 실행 안 됨
+
+myEvent.on("event4", () => {
+  console.log("이벤트 4");
+});
+myEvent.removeAllListeners("event4"); // 이벤트 리스너 모두 제거
+myEvent.emit("event4"); // 실행 안 됨
+
+const listener = () => {
+  console.log("이벤트 5");
+};
+myEvent.on("event5", listener); // 리스너를 만들고
+myEvent.removeListener("event5", listener); // 리스너를 제거
+myEvent.emit("event5"); // 실행 안 됨
+
+console.log(myEvent.listenerCount("event2")); // 이벤트에 연결된 리스너 개수를 확인합니다.
