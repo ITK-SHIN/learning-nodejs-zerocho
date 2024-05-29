c5c4bd628f8d9575dccda775192c315dc414b56a
Shin Sangwoo <bano112@naver.com>
2024-05-29 17:40:50 +0900

    package.json 살펴보기

---
+++ lecture/5_npm/protocol.js
@@ -0,0 +1,3 @@
+console.log(require.resolve("fs")); // fs
+console.log(require.resolve("express")); // c:\Users\PC\OneDrive\바탕 화면\codingFolder\nodejs\node_zerocho\lecture\5_npm\node_modules\express\index.js
+console.log(require.resolve("node:fs")); // node:fs
