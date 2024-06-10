675ddd88f04fa851b016a4c9d87b2bcd565e25b2
Shin Sangwoo <bano112@naver.com>
2024-06-10 20:30:40 +0900

    MySQL 연결성공

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -3,13 +3,44 @@ const path = require("path");
 const morgan = require("morgan");
 const nunjucks = require("nunjucks");
 
-const { sequelize } = require("./models");
+const { sequelize } = require("./models"); //require('./models/index.js')와 같다. index.js는 생략 가능
 
-const app = express();
-app.set("port", process.env.PORT || 3001);
-app.set("view engine", "html");
+const app = express(); // express 객체 생성
+app.set("port", process.env.PORT || 3001); // PORT 환경변수가 설정되어 있지 않다면 3001을 사용
+app.set("view engine", "html"); // 템플릿 엔진을 html로 설정
 // views 폴더를 템플릿 폴더로 지정
 nunjucks.configure("views", {
   express: app,
   watch: true, // 파일이 변경될 때 템플릿 엔진을 다시 로드
+});
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan("dev")); // 개발 시 사용
+app.use(express.static(path.join(__dirname, "public"))); // 정적 파일 제공
+app.use(express.json()); // JSON 데이터 처리
+app.use(express.urlencoded({ extended: false })); // form 데이터 처리
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error); // 에러 처리 미들웨어로 전달
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV != "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
 });
--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -1,37 +1,44 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const fs = require("fs"); // 파일 시스템 모듈
+const path = require("path"); // 경로 관련 모듈
+const Sequelize = require("sequelize"); // 시퀄라이즈 패키지
+const process = require("process"); // process.env.NODE_ENV를 사용하기 위해 추가
+const basename = path.basename(__filename); // 현재 파일명을 담는다.
+const env = process.env.NODE_ENV || "development"; // NODE_ENV 환경변수가 설정되어 있지 않다면 development를 사용
+const config = require(__dirname + "/../config/config.json")[env]; // config.json 파일을 불러온다.
+const db = {}; // db 객체 생성
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config); // 환경변수로 연결
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
